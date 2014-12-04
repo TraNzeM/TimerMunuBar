089d73a9f7210374875aaffdda9983fcca1f65d8
Ivan (TraNzeM) Zyablov <ivanzyablov@gmail.com>
2014-12-04 22:02:47 +0500

    Initial Commit

---
+++ TestMenuBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestMenuBar
+//
+//  Created by Ivan Zyablov on 04.12.14.
+//  Copyright (c) 2014 TraNzeM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
